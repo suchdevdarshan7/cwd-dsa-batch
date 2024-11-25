961a80f638483c2de8414c7c94555006acc4f882
Suchdev Darshan <suchdev7@gmail.com>
2024-11-26 00:40:35 +0530

    Day 1 updated

---
+++ day1-cpp/datatypes.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a = 10;
+    float b = 3.14f;
+    double c = 3.324908;
+    char d = 'A'; // use single quoatation in char
+    bool e = true;
+
+    cout << a << endl;
+    cout << b << endl;
+    cout << c << endl;
+    cout << d << endl;
+    cout << e << endl;
+
+    return 0;
+}
---
+++ day1-cpp/example.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // int a;
+    // cout << "Enter a number " << endl;
+    // cin >> a;
+
+    // cout << "The number which was entered is" << a;
+    // return 0;
+
+    cout << "Hi" << endl;
+    cout << "Hello" << endl;
+}
